e112a5a4c99283f59a095144b045948804affddc
Mohit Kumar <Itsmohit.kr95@gmail.com>
2020-04-01 23:44:15 +0530

    Add files via upload

---
+++ source_pytorch/model.py
@@ -0,0 +1,67 @@
+# torch imports
+import torch.nn.functional as F
+import torch.nn as nn
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim as optim
+
+
+## TODO: Complete this classifier
+class BinaryClassifier(nn.Module):
+    """
+    Define a neural network that performs binary classification.
+    The network should accept your number of features as input, and produce 
+    a single sigmoid value, that can be rounded to a label: 0 or 1, as output.
+    
+    Notes on training:
+    To train a binary classifier in PyTorch, use BCELoss.
+    BCELoss is binary cross entropy loss, documentation: https://pytorch.org/docs/stable/nn.html#torch.nn.BCELoss
+    """
+
+    ## TODO: Define the init function, the input params are required (for loading code in train.py to work)
+    def __init__(self, input_features, hidden_dim, output_dim):
+        """
+        Initialize the model by setting up linear layers.
+        Use the input parameters to help define the layers of your model.
+        :param input_features: the number of input features in your training/test data
+        :param hidden_dim: helps define the number of nodes in the hidden layer(s)
+        :param output_dim: the number of outputs you want to produce
+        """
+        super(BinaryClassifier, self).__init__()
+        
+        self.input_features = input_features
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        
+        self.fc1 = nn.Linear(self.input_features, 50)
+        self.relu1 = nn.ReLU()
+        self.dout = nn.Dropout(0.2)
+        self.fc2 = nn.Linear(50, 100)
+        self.prelu = nn.PReLU(1)
+        self.out = nn.Linear(100, self.output_dim)
+        self.out_act = nn.Sigmoid()
+
+        # define any initial layers, here
+        
+
+    
+    ## TODO: Define the feedforward behavior of the network
+    def forward(self, x):
+        """
+        Perform a forward pass of our model on input features, x.
+        :param x: A batch of input features of size (batch_size, input_features)
+        :return: A single, sigmoid-activated value as output
+        """
+        
+        a1 = self.fc1(x)
+        h1 = self.relu1(a1)
+        dout = self.dout(h1)
+        a2 = self.fc2(dout)
+        h2 = self.prelu(a2)
+        a3 = self.out(h2)
+        y = self.out_act(a3)
+        
+        return y
+    
